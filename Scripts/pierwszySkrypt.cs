441dfe7bca516794a8081beaab5cf46a541ef3ef
filipwroblewski <filip.wroblewski.row@gmail.com>
2022-11-10 08:16:35 +0100

    pierwszy skrypt

---
+++ Scripts/pierwszySkrypt.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pierwszySkrypt : MonoBehaviour
+{
+    // typ_zmiennej tekst  = "abcd";
+    string tekst = "Dziala";
+    decimal liczba1 = 3;
+    double liczba2 = 3.5;
+    float liczba3 = 1.2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(tekst);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("sus");
+    }
+}
