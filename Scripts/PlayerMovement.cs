015d84236a3e77a9219e54bff06cd64ee5ec854f
Filip Wróblewski <75478989+filipwroblewski@users.noreply.github.com>
2022-12-22 17:46:30 +0100

    Add files via upload

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -4,30 +4,45 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D rb2d;
+    Animator anim;
+
     float jumpHeight = 4.0f;
     float movementSpeed = 4.0f;
-    Rigidbody2D rb2d;
     float osX;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         // Horizontal
         osX = Input.GetAxisRaw("Horizontal");
-        Debug.Log(osX);
-        rb2d.velocity = new Vector2(osX * movementSpeed, rb2d.velocity.y);
-
-
+        // Debug.Log(osX);
+        rb2d.velocity = new Vector2(osX * movementSpeed, rb2d.velocity.y); //4.0f -> movementSpeed
 
         if (Input.GetButton("Jump")) // Input.GetKeyDown("space")
         {
             //GetComponent<Rigidbody2D>().velocity = new Vector3(0, 2, 0);
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpHeight); // 0 -> rb2d.velocity.x
             //Debug.Log("spacja kliknieta");
+        }
+
+        UpdateAnimationState();
+    }
+
+    void UpdateAnimationState()
+    {
+        if (osX > 0 || osX < 0) // osX > 0 bieg w prawo; osX < 0 bieg w lewo
+        {
+            anim.SetBool("running", true);
+        }
+        else
+        {
+            anim.SetBool("running", false);
         }
     }
 }
